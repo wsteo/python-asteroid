2871a0c73aceb7585992a1e95d844773df6b3213
wsteo <35697328+wsteo@users.noreply.github.com>
2025-01-06 14:33:55 +0800

    add rotation to the player

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
-    
+
     x = SCREEN_WIDTH / 2
     y = SCREEN_HEIGHT / 2
     player = Player(x, y)
@@ -20,6 +20,7 @@ def main():
                 return
             
         screen.fill(BLACK)
+        player.update(dt)
         player.draw(screen)
         pygame.display.flip()
 
--- player.py
+++ player.py
@@ -1,5 +1,5 @@
 import pygame
-from constants import PLAYER_RADIUS
+from constants import PLAYER_RADIUS,PLAYER_TURN_SPEED
 from circleshape import CircleShape
 
 class Player(CircleShape):
@@ -18,3 +18,15 @@ class Player(CircleShape):
     def draw(self, screen):
         WHITE = (255, 255, 255, 1)
         pygame.draw.polygon(screen, WHITE, self.triangle(), 2)
+
+    def rotate(self, dt):
+        self.rotation += PLAYER_TURN_SPEED * dt
+
+    def update(self, dt):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_a]:
+            self.rotate(-dt)
+        
+        if keys[pygame.K_d]:
+            self.rotate(dt)
